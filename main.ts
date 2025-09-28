a955378f6a9956e30a4cf1df0a2347205a791c2a
noah-quetua <noah.quetua@stu.ocsb.ca>
2025-09-28 22:31:41 +0000

    added main.ts for assignment

---
+++ main.ts
@@ -0,0 +1,30 @@
+/**
+ * @author Noah Quetua
+ * @version 1.0.0
+ * @date 9/28/2025
+ * @fileoverview This program shows the conversion of celsius to Fahrenheit, The area of a circle, the volume of a sphere, the gross pay for an employee
+ */
+
+//Celcius to Fahrenheit
+console.log("F = 9 / 5 * C + 32");
+console.log("9 / 5 * 34 + 32 = " + (9 / 5 * 34 + 32))
+
+console.log("\nDone");
+
+//Area of a circle
+console.log("A = Pi * r * r");
+console.log("3.14 * 14 * 14 = " + (3.14 * 14 * 14));
+
+console.log("\nDone")
+
+//Volume of a sphere
+console.log("V = 4 / 3 * Pi * r * r * r");
+console.log("4 / 3 * 3.14 * 4 * 4 * 4 = " + (4 / 3 * 3.14 * 4 * 4 * 4));
+
+console.log("\nDone")
+
+//Gross pay of an employee
+console.log("gross pay = hours worked * hourly wage");
+console.log("40 * 8.2 = " + (40 * 8.2));
+
+console.log("\nDone");
